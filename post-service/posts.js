0dcf9e4b202b6a8940e8b5dc3a4eddd4fc9d612e
yusufyi <yusufyi@hotmail.com>
2024-03-29 14:51:01 +0000

    Post Service

---
+++ post-service/posts.js
@@ -0,0 +1,24 @@
+// post-service/posts.js
+
+const posts = [
+  {
+    id: 1,
+    userId: 1,
+    title: "First Post",
+    content: "Content of the first post.",
+  },
+  {
+    id: 2,
+    userId: 1,
+    title: "Second Post",
+    content: "Content of the second post.",
+  },
+  {
+    id: 3,
+    userId: 2,
+    title: "Third Post",
+    content: "Content of the third post.",
+  },
+];
+
+module.exports = posts;
