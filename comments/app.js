a03eb02b8112dc71f18203a584f16ceefa018bd3
Yusuf Yigit <39989409+yusufyi@users.noreply.github.com>
2024-03-31 14:46:14 +0100

    comments

---
+++ comments/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+const port = 4001;
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: comments.length + 1, content });
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(port, () => {
+  console.log(`Comments service is running on ${port}`);
+});
+// Path: eventbus/eventbus.js
+// Compare this snippet from auth-service/app.js:
